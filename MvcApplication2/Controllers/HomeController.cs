4d06e749063e70d0d6afa6df06d62426b573b586
rockguy <rockguy112@gmail.com>
2016-05-24 11:24:07 +0800

    It's work without authorize

---
+++ MvcApplication2/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using IrkIdea.Models;
+using System.Data;
+
+
+namespace IrkIdea.Controllers
+{
+    public class HomeController : Controller
+    {
+        
+        Context db = new Context();
+        
+       
+        public ActionResult Index()
+        {
+         
+            ViewBag.Events = db.UsersEvents;
+            ViewBag.Name = HttpContext.User.Identity.Name;
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult New_Event()
+        {
+            return View();
+        }
+        [HttpPost]
+        public string New_Event(UserEvent Ue)
+        {
+            Ue.DateOfCreating = DateTime.Now;
+            db.Entry(Ue).State = EntityState.Added;
+            db.SaveChanges();
+            return "Nice job";
+        }
+       
+    }
+}
---
+++ MvcApplication2/Models/UserEvent.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using System.Data.Metadata;
+
+
+namespace IrkIdea.Models
+{
+    public class UserEvent:DbContext
+    {
+        
+        public int Id { get; set; }
+        public string What { get; set; }
+        public string Where { get; set; }
+        public DateTime When { get; set; }
+        public DateTime DateOfCreating { get; set; }
+
+
+    }
+}
